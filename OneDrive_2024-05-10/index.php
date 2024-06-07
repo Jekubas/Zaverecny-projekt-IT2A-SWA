58f5d6dbd018d562bad0d6f33d07c8216782a0e1
Jakub Jegla <144122026+Jekubas@users.noreply.github.com>
2024-06-07 21:23:53 +0200

    index.php

--- OneDrive_2024-05-10/index.php
+++ OneDrive_2024-05-10/index.php
@@ -1,32 +1,4 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "Arsenal";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-
-// sql to create table
-$sql = "CREATE TABLE Fotbal (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-firstname VARCHAR(30) NOT NULL,
-lastname VARCHAR(30) NOT NULL,
-email VARCHAR(50),
-reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-  echo "Table Fotbal created successfully";
-} else {
-  echo "Error creating table: " . $conn->error;
-}
-
-$conn->close();
 ?>
 <html>
 <head>
